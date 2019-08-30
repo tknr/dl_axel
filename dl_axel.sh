94f09dea35aa5b3efd5c9ab6a488825a3ab15df4
tknr <geishagirlskoume@gmail.com>
2019-08-30 22:38:52 +0900

    2019年  8月 30日 金曜日 22:38:52 JST

---
+++ dl_axel.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+export IFS=$'\n'
+DIR=$(cd $(dirname $0); pwd)
+cd $DIR
+CMDNAME=`basename $0`
+FILENAME_QUEUE="queue_axel.txt"
+if [ $# -ne 0 ]; then
+    ${FILENAME_QUEUE}=${1}
+fi
+echo "reading from "${FILENAME_QUEUE}"..."
+
+for LINE in `cat ${FILENAME_QUEUE} | grep -v "^#"`
+do
+
+        TITLE=`echo ${LINE} | cut -d "," -f 1`
+	URL=`echo ${LINE} | cut -d "," -f 2`
+
+	if [ ${TITLE} = ${URL} ] ; then 
+		TITLE=''
+	fi
+
+
+	echo ${TITLE}
+	echo ${URL}
+
+	echo ${#TITLE}
+	echo ${#URL}
+
+        if [ ${#URL} -lt 2 ] ; then
+                continue
+        fi
+
+	if [ ${#TITLE} = 0 ] ; then
+		axel -a -v -n 10 ${URL} || continue
+	else
+		axel -a -v -n 10 -o ${TITLE} ${URL} || continue
+	fi
+
+	sed -i -e "s|${LINE}|#${LINE}|g" ${FILENAME_QUEUE}
+done
