1bd42b565957cd3f0fb1d0010c7c4dcde9f4c45f
tknr <geishagirlskoume@gmail.com>
2020-11-19 13:39:59 +0900

    2020年 11月 19日 木曜日 13:39:59 JST

--- dl_axel.sh
+++ dl_axel.sh
@@ -3,11 +3,14 @@ export IFS=$'\n'
 DIR=$(cd $(dirname $0); pwd)
 cd $DIR
 CMDNAME=`basename $0`
+
 FILENAME_QUEUE="queue_axel.txt"
 if [ $# -ne 0 ]; then
     ${FILENAME_QUEUE}=${1}
 fi
 echo "reading from "${FILENAME_QUEUE}"..."
+
+NUM_CONNECTIONS=10
 
 for LINE in `cat ${FILENAME_QUEUE} | grep -v "^#"`
 do
@@ -34,9 +37,9 @@ do
 	TARGET_URL=`curl -I -Ls -o /dev/null -w %{url_effective} ${URL}`
 
 	if [ ${#TITLE} = 0 ] ; then
-		axel -a -v -n 10 ${TARGET_URL} || continue
+		axel -a -v -n ${NUM_CONNECTIONS} ${TARGET_URL} || continue
 	else
-		axel -a -v -n 10 -o ${TITLE} ${TARGET_URL} || continue
+		axel -a -v -n ${NUM_CONNECTIONS} -o ${TITLE} ${TARGET_URL} || continue
 	fi
 
 	sed -i -e "s|${LINE}|#${LINE}|g" ${FILENAME_QUEUE}
